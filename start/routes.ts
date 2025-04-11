56b6f58dc7143acfc3d8d444956bc22ed045f168
JanekDr <jan.druszcz@onet.eu>
2025-04-11 02:09:28 +0200

    fix: fix code style issues

--- start/routes.ts
+++ start/routes.ts
@@ -18,5 +18,7 @@ router.get("/", async () => {
 });
 
 router.post("api/v1/search-queries", [SearchQueriesController, "store"]);
+
 router.get("api/v1/search-queries", [SearchQueriesController, "index"]);
+
 router.get("api/v1/search-queries/:id", [SearchQueriesController, "show"]);
