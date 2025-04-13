6daafb51bf1cc91f52dea28deaa3b58fd0f40ecb
JanekDr <jan.druszcz@onet.eu>
2025-04-13 19:07:18 +0200

    feat: add email field in search query

--- app/models/search_query.ts
+++ app/models/search_query.ts
@@ -24,6 +24,9 @@ export default class SearchQuery extends BaseModel {
   @column()
   declare districtId: number;
 
+  @column()
+  declare email: string | null;
+
   @column.dateTime()
   declare refreshedAt: DateTime;
 
